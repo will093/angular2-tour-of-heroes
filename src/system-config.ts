b85a3cfa5e3683b74e0e81b895e817157717a01f
William Taylor <William Taylor@cadsltd.local>
2016-07-25 14:08:53 +0100

    Add angular2-in-memory-web-api dependency

--- src/system-config.ts
+++ src/system-config.ts
@@ -9,41 +9,45 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
-  'angular2-in-memory-web-api': 'vendor/angular2-in-memory-web-api/index.js'
+    'angular2-in-memory-web-api': 'vendor/angular2-in-memory-web-api'
 };
 
 /** User packages configuration. */
 const packages: any = {
-
+    'angular2-in-memory-web-api': {
+        main: 'index.js',
+        defaultExtension: 'js'
+    },
 };
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.
  **********************************************************************************************/
 const barrels: string[] = [
-  // Angular specific barrels.
-  '@angular/core',
-  '@angular/common',
-  '@angular/compiler',
-  '@angular/forms',
-  '@angular/http',
-  '@angular/router',
-  '@angular/platform-browser',
-  '@angular/platform-browser-dynamic',
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
 
-  // Thirdparty barrels.
-  'rxjs',
+    // Thirdparty barrels.
+    'rxjs',
 
-  // App specific barrels.
-  'app',
-  'app/shared',
-  /** @cli-barrel */
+    // App specific barrels.
+    'app',
+    'app/shared',
+    /** @cli-barrel */
 ];
 
 const cliSystemConfigPackages: any = {};
 barrels.forEach((barrelName: string) => {
-  cliSystemConfigPackages[barrelName] = { main: 'index' };
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
 });
 
 /** Type declaration for ambient System. */
@@ -51,12 +55,12 @@ declare var System: any;
 
 // Apply the CLI SystemJS configuration.
 System.config({
-  map: {
-    '@angular': 'vendor/@angular',
-    'rxjs': 'vendor/rxjs',
-    'main': 'main.js'
-  },
-  packages: cliSystemConfigPackages
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
 });
 
 // Apply the user's configuration.
